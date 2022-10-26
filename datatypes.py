994ae19d7d7e017b89c42141dbb9ab38bfc6e520
Douglas Wanjohi <dwanj@TYSON.risingfreight.co.ke>
2022-10-26 21:32:22 +0300

    python2

---
+++ datatypes.py
@@ -0,0 +1,34 @@
+#datatypes in python
+
+#primitive datatypes
+
+from time import process_time_ns
+from types import NoneType
+
+
+x=5
+y=2.5
+
+#python allows varaiblr decleration on the same line
+#x,y=5,2.5
+
+#python allows multiple variables to the same
+a=b=c=3
+
+my_string=("'Hello Naruto!")#str data type
+is_true=True #Boolean data type
+is_false= False #Boolean data type
+my_first_list=[1,1,2,3]#list data type
+none_type= None
+
+my_char=('a')
+
+print(type(x))
+print(type(y))
+print(type(my_string))
+print(type(is_true))
+print(type(none_type))
+print(type(my_first_list))
+
+
+print
---
+++ output.py
@@ -0,0 +1,7 @@
+#output in python
+# the print ()functipn
+print("Hello Shinobi")
+
+#PYTHON STATMENTS HAVE NO INTERPRETER
+print(2+2)
+print(10 % 3)
